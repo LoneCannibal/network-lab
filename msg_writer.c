7f95fdbb8447bb5cece4e161f3f8a50302c15668
LoneCannibal <aakashsd2000@gmail.com>
2021-06-25 19:45:15 +0530

    Message queue program done

--- msg_writer.c
+++ msg_writer.c
@@ -1,11 +1,11 @@
-//Program to implement interprocess communication using message queue
+//Program to implement interprocess communication using message queue (Writer program)
 #include<stdio.h>
 #include<sys/ipc.h>
 #include<sys/msg.h>
-char string1[]="Hello";
+#include<stdlib.h>
 struct msg_queue{
     long msg_type;
-    char msg_txt[100];
+    char msg_text[100];
 }message;
 void main(){
     int key,msg_id;
@@ -16,8 +16,8 @@ void main(){
         exit(1);
     }
     message.msg_type=1;
-    printf("Write a word: ");
-    scanf("%s",message.msg_txt);
+    printf("Write a sentence: ");
+    fgets(message.msg_text,sizeof(message.msg_text),stdin);
     msgsnd(msg_id,&message,sizeof(message),0);
 
 }
