559d2a92e7adfe365379e0cb0147ba43009a9983
Lonecannibal <aakashsd2000@gmail.com>
2021-07-07 14:56:06 +0530

    UDP client and server working

---
+++ udp_client.c
@@ -0,0 +1,33 @@
+//Program to implement client server communication using UDP
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define PORT 5000
+struct sockaddr_in clientaddr;
+void chat(int sockfd){
+    while(1){
+        char buffer[200];
+        struct sockaddr_in servaddr;
+        printf("Client: ");
+        scanf("%[^\n]%*c", buffer);
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &clientaddr,sizeof(clientaddr));
+        strcpy(buffer,"");
+        recvfrom(sockfd, buffer, sizeof(buffer), MSG_WAITALL, ( struct sockaddr *)NULL,NULL);
+        printf("Server: %s [MSG LENGTH: %d]\n",buffer,strlen(buffer));
+    }
+}
+int main() {
+    
+    int sockfd=socket(AF_INET,SOCK_DGRAM,0);   
+    if(sockfd==-1){
+        printf("Socket creation failed\n");
+        exit(1);
+    }
+    printf("Socket created successfully");
+    clientaddr.sin_family=AF_INET;
+    clientaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    clientaddr.sin_port=htons(PORT);
+    chat(sockfd);
+}
--- udp_server.c
+++ udp_server.c
@@ -1,34 +1,38 @@
-//Program to implement client server communication using UDP
+/*Program to implement client server communication using UDP*/
 #include<stdio.h>
 #include<stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define PORT 8080
+#define PORT 5000
+struct sockaddr_in servaddr;
 void chat(int sockfd){
     while(1){
         char buffer[200];
         struct sockaddr_in cliaddr;
-        int n=recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, ( struct sockaddr *)&cliaddr,sizeof(cliaddr));
-        printf("Client: %s\n",buffer);
+        int len=sizeof(cliaddr);
+        int n=recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, ( struct sockaddr *)&cliaddr,&len);
+        buffer[n]='\0';
+        printf("Client: %s [MSG LENGTH: %d]\n",buffer,strlen(buffer));
+
         printf("Server: ");
         scanf("%[^\n]%*c", buffer);
-        sendto(sockfd, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &cliaddr,sizeof(cliaddr));
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cliaddr,sizeof(cliaddr));
         strcpy(buffer,"");
     }
-
 }
 int main(){
-    struct sockaddr_in servaddr;
+    
     int sockfd=socket(AF_INET,SOCK_DGRAM,0);   
     if(sockfd==-1){
-        printf("Socker creation failed\n");
+        printf("Socket creation failed\n");
         exit(1);
     }
+    printf("Socket created successfully\n");
     servaddr.sin_family=AF_INET;
-    servaddr.sin_addr.s_addr=INADDR_ANY;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
     servaddr.sin_port=htons(PORT);
-    if((bind(sockfd,&servaddr,sizeof(servaddr)))==-1)
+    if((bind(sockfd,( struct sockaddr *)&servaddr,sizeof(servaddr)))==-1)
     {
         printf("Failed to bind socket with IP and port\n");
         exit(0);
